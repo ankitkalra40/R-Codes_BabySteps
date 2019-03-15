68ef2e13546a1135d67c5c2cbdd45ada0784eaa9
guest <ankitkalra40@gmail.com>
2019-03-15 21:30:21 +0530

    My Second Code NiBBas

---
+++ first-script_matrixincluded.R
@@ -0,0 +1,16 @@
+#vector
+# numeric vector
+> A= c(1,2,3,4,5)
+> B= c(3,4,5,6,7)
+# character vector
+> C= c('a','b','c','d')
+> D= c("a","b","c")
+> E= c('Ankit', 'Rahul', "Shashank")
+> F= c(1,2,'Ankit')
+> G= c(5,4,6)
+> A*B*G
+
+#matrix
+> mat= matrix(c(1,2,3,4))
+> mat2 = matrix(c(1,2,3,4), ncol=2)
+
