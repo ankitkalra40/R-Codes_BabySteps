f628609eea0f899f28c92a93f458fdeb78c609e8
guest <ankitkalra40@gmail.com>
2019-03-15 20:48:07 +0530

    My First Code NiBBas

---
+++ first-script.R
@@ -0,0 +1,13 @@
+#vector
+# numeric vector
+> A= c(1,2,3,4,5)
+> B= c(3,4,5,6,7)
+# character vector
+> C= c('a','b','c','d')
+> D= c("a","b","c")
+> E= c('Ankit', 'Rahul', "Shashank")
+> F= c(1,2,'Ankit')
+> G= c(5,4,6)
+> A*B*G
+
+
